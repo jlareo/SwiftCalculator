65a79276afeb3c8126f29b61ee7bc25a45e4202f
jlareo <lareomerlin@gmail.com>
2015-07-22 10:45:55 +0100

    Add buttons label and some logic

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    var isTypingNumber = false
+    var firstNumber = 0
+    var secondNumber = 0
+    var operation = ""
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func numberPressed(sender: AnyObject) {
+        var number = sender.currentTitle
 
+        if isTypingNumber {
+            displayLabel.text! +=   number!!
+        }
+        else{
+            displayLabel.text = number
+            isTypingNumber = true
+        }
+    }
 
+    @IBAction func operatorButtonPressed(sender: AnyObject) {
+        isTypingNumber = false
+        firstNumber = displayLabel.text!.toInt()!
+        operation = sender.currentTitle!!
+    }
+
+    @IBAction func equalsButtonPressed(sender: AnyObject) {
+        isTypingNumber = false
+        var result = 0
+        secondNumber = displayLabel.text!.toInt()!
+        
+        if operation == "+"{
+            result = firstNumber + secondNumber
+        } else if operation == "-" {
+            result = firstNumber - secondNumber
+        }
+
+        displayLabel.text = "\(result)"
+    }
 }
 
